a0feb3a45291039bae077f5a54ae9a131751b05c
GabriellyAssuncao <gabriellyassuncaorodrigues@gmail.com>
2025-01-09 10:21:48 -0300

    configurações do ambiente

--- dml/insert_data.sql
+++ dml/insert_data.sql
@@ -1,4 +1,5 @@
   -- Arquivo: ddl/insert_data.sql
 
-  INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
-  INSERT INTO users (name, email) VALUES ('Alice Wonderland', 'alice.wonderland@example.com');
+  -- Exemplo
+  -- INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+  -- INSERT INTO users (name, email) VALUES ('Alice Wonderland', 'alice.wonderland@example.com');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -24,27 +24,45 @@ async function executeSQLFile(filePath) {
     throw error;
   }
 }
-
-async function init() {
-  console.log('Resetando banco de dados...');
-  await executeSQLFile(path.join(__dirname, '../ddl/reset_db.sql'));
-
-  console.log('Criando tabelas...');
-  await executeSQLFile(path.join(__dirname, '../ddl/create_tables.sql'));
-
-  console.log('Inserindo dados...');
-  await executeSQLFile(path.join(__dirname, '../dml/insert_data.sql'));
-
-  console.log('Dados inseridos com sucesso.');
+async function waitForDatabase() {
+  let retries = 10;
+  while (retries > 0) {
+    try {
+      console.log('Verificando se o banco de dados está pronto...');
+      await pool.query('SELECT 1'); // Testa a conexão
+      console.log('Banco de dados está pronto!');
+      return;
+    } catch (err) {
+      console.log('Banco de dados não está pronto, aguardando...');
+      retries--;
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
+  throw new Error('O banco de dados não inicializou dentro do tempo esperado.');
 }
 
+async function init() {
+  try {
+    await waitForDatabase();
+    console.log('Resetando banco de dados...');
+    await executeSQLFile(path.join(__dirname, '../ddl/reset_db.sql'));
 
-init()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Servidor rodando na porta ${port}`);
-    });
-  })
-  .catch((err) => {
+    console.log('Criando tabelas...');
+    await executeSQLFile(path.join(__dirname, '../ddl/create_tables.sql'));
+
+    // deixar comentado por enquanto
+    // console.log('Inserindo dados...');
+    // await executeSQLFile(path.join(__dirname, '../dml/insert_data.sql'));
+
+    console.log('Dados inseridos com sucesso.');
+  } catch (err) {
     console.error('Erro na inicialização:', err);
+    process.exit(1);
+  }
+}
+
+init().then(() => {
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
   });
+});
