3573ba92316619f4d58c242564a9deba7d3e3293
GabriellyAssuncao <gabriellyassuncaorodrigues@gmail.com>
2025-01-11 16:40:04 -0300

    corrigindo tabelas

--- ddl/create_tables.sql
+++ ddl/create_tables.sql
@@ -1,59 +1,59 @@
 -- Criando as sequências primeiro
-CREATE SEQUENCE mapa_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE regiao_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE sala_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE exohumano_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE missao_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE puzzle_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE dialogo_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE cyberlutador_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE instancia_item_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE item_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE npc_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE componente_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE faccao_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE mercado_clandestino_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE mochila_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE carro_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE player_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE inimigo_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE instancia_inimigo_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE netrunners_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE codekeepers_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE voidwalkers_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE implante_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE VisaoCibernetica START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE BracoRobotico START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE CapaceteNeural START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS mapa_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS regiao_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS sala_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS exohumano_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS missao_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS puzzle_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS dialogo_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS cyberlutador_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS instancia_item_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS item_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS npc_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS componente_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS faccao_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS mercado_clandestino_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS mochila_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS carro_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS player_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS inimigo_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS instancia_inimigo_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS netrunners_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS codekeepers_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS voidwalkers_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS implante_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS visaocibernetica START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS bracorobotico START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS capaceteneural START WITH 1 INCREMENT BY 1;
 
 -- Agora criando as tabelas que dependem das sequências
-CREATE TABLE Mapa(
+CREATE TABLE IF NOT EXISTS Mapa(
   idMapa INT PRIMARY KEY DEFAULT nextval('mapa_id_seq'),
   nomeMapa VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Regiao(
+CREATE TABLE IF NOT EXISTS Regiao(
   idRegiao INT PRIMARY KEY DEFAULT nextval('regiao_id_seq'),
   nomeRegiao VARCHAR(50) NOT NULL,
   fk_mapa INT NOT NULL,
   FOREIGN KEY (fk_mapa) REFERENCES Mapa (idMapa)
 );
 
-CREATE TABLE Sala(
+CREATE TABLE IF NOT EXISTS Sala(
   idSala INT PRIMARY KEY DEFAULT nextval('sala_id_seq'),
   nomeSala VARCHAR(50) NOT NULL,
   fk_regiao INT NOT NULL,
   FOREIGN KEY (fk_regiao) REFERENCES Regiao (idRegiao)
 );
 
-CREATE TABLE ExoHumano (
+CREATE TABLE IF NOT EXISTS ExoHumano (
   idExoHumano INT PRIMARY KEY DEFAULT nextval('exohumano_id_seq'),
   nome VARCHAR(50) NOT NULL,
   fk_sala INT NOT NULL,
   FOREIGN KEY (fk_sala) REFERENCES Sala (idSala)
 );
 
-CREATE TABLE Missao (
+CREATE TABLE IF NOT EXISTS Missao (
   idMissao INT PRIMARY KEY DEFAULT nextval('missao_id_seq'),
   nomeMissao VARCHAR(50) NOT NULL,
   recompensa INT NOT NULL,
@@ -65,7 +65,7 @@ CREATE TABLE Missao (
   FOREIGN KEY (fk_exohumano) REFERENCES ExoHumano (idExoHumano)
 );
 
-CREATE TABLE Puzzle (
+CREATE TABLE IF NOT EXISTS Puzzle (
   idPuzzle INT PRIMARY KEY DEFAULT nextval('puzzle_id_seq'), 
   nomePuzzle VARCHAR(30) NOT NULL,
   dificuldade CHAR(10) NOT NULL,
@@ -74,27 +74,27 @@ CREATE TABLE Puzzle (
 );
 
 -- Subclasses
-CREATE TABLE Matematico (
+CREATE TABLE IF NOT EXISTS Matematico (
   fk_puzzle INT NOT NULL,
   expressao VARCHAR(50) NOT NULL,
   FOREIGN KEY (fk_puzzle) REFERENCES Puzzle (idPuzzle) ON DELETE CASCADE
 );
 
-CREATE TABLE Decodificar (
+CREATE TABLE IF NOT EXISTS Decodificar (
   fk_puzzle INT NOT NULL,
   codigo VARCHAR(50) NOT NULL,
   FOREIGN KEY (fk_puzzle) REFERENCES Puzzle (idPuzzle) ON DELETE CASCADE
 );
 
-CREATE TABLE Dialogo (
+CREATE TABLE IF NOT EXISTS Dialogo (
   idDialogo INT PRIMARY KEY DEFAULT nextval('dialogo_id_seq'),
   nomeDialogo VARCHAR(30) NOT NULL,
   fk_exohumano INT NOT NULL,
   FOREIGN KEY (fk_exohumano) REFERENCES ExoHumano (idExoHumano)
 );
 
-CREATE TABLE CyberLutador (
-  idCyberLadder INT PRIMARY KEY DEFAULT nextval('cyberlutador_id_seq'),
+CREATE TABLE IF NOT EXISTS CyberLutador (
+  idCyberLutador INT PRIMARY KEY DEFAULT nextval('cyberlutador_id_seq'),
   inteligencia INT NOT NULL,
   resistencia INT NOT NULL,
   furtividade INT NOT NULL,
@@ -107,33 +107,33 @@ CREATE TABLE CyberLutador (
   FOREIGN KEY (fk_inimigo) REFERENCES ExoHumano (idExoHumano)
 );
 
-CREATE TABLE Item (
+CREATE TABLE IF NOT EXISTS Item (
   idItem INT PRIMARY KEY DEFAULT nextval('item_id_seq'),
   nomeItem VARCHAR(50) NOT NULL,
   descricao VARCHAR(255) NOT NULL,
   valor INT NOT NULL
 );
 
-CREATE TABLE InstanciaItem (
+CREATE TABLE IF NOT EXISTS InstanciaItem (
   idInstanciaItem INT PRIMARY KEY DEFAULT nextval('instancia_item_id_seq'),
   fk_item INT NOT NULL,
   FOREIGN KEY (fk_item) REFERENCES Item (idItem)
 );
 
-CREATE TABLE NPC (
+CREATE TABLE IF NOT EXISTS NPC (
   idNPC INT PRIMARY KEY DEFAULT nextval('npc_id_seq'),
   nomeNPC VARCHAR(50) NOT NULL,
   fk_sala INT NOT NULL,
   FOREIGN KEY (fk_sala) REFERENCES Sala (idSala)
 );
 
-CREATE TABLE Componente (
+CREATE TABLE IF NOT EXISTS Componente (
   idComponente INT PRIMARY KEY DEFAULT nextval('componente_id_seq'),
   nomeComponente VARCHAR(50) NOT NULL,
   tipo VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Faccao (
+CREATE TABLE IF NOT EXISTS Faccao (
   idFaccao INT PRIMARY KEY DEFAULT nextval('faccao_id_seq'),
   nomeFaccao VARCHAR(50) NOT NULL,
   ideologia VARCHAR(255) NOT NULL,
@@ -143,7 +143,7 @@ CREATE TABLE Faccao (
   especialidade VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE MercadoClandestino (
+CREATE TABLE IF NOT EXISTS MercadoClandestino (
   idMercadoClandestino INT PRIMARY KEY DEFAULT nextval('mercado_clandestino_id_seq'),
   nomeMercado VARCHAR(50) NOT NULL,
   produtoFornecido VARCHAR(50) NOT NULL,
@@ -151,7 +151,7 @@ CREATE TABLE MercadoClandestino (
   FOREIGN KEY (fk_sala) REFERENCES Sala (idSala)
 );
 
-CREATE TABLE Mochila (
+CREATE TABLE IF NOT EXISTS Mochila (
   idMochila INT PRIMARY KEY DEFAULT nextval('mochila_id_seq'),
   capacidade INT NOT NULL,
   fk_player INT NOT NULL,
@@ -160,7 +160,7 @@ CREATE TABLE Mochila (
   FOREIGN KEY (fk_item) REFERENCES Item (idItem)
 );
 
-CREATE TABLE Carro(
+CREATE TABLE IF NOT EXISTS Carro(
   idCarro INT PRIMARY KEY DEFAULT nextval('carro_id_seq'),
   capacidade INT NOT NULL,
   velocidade INT NOT NULL,
@@ -171,11 +171,11 @@ CREATE TABLE Carro(
   blindagem INT NOT NULL,
   fk_regiao INT NOT NULL,
   fk_mercado_clandestino INT NOT NULL,
-  FOREIGN KEY (fk_regiao) REFERENCES Mapa (idRegiao),
-  FOREIGN KEY (fk_mercado_clandestino) REFERENCES MercadoClandestino (idMercadoClandestino),
+  FOREIGN KEY (fk_regiao) REFERENCES Regiao (idRegiao),
+  FOREIGN KEY (fk_mercado_clandestino) REFERENCES MercadoClandestino (idMercadoClandestino)
 );
 
-CREATE TABLE Player(
+CREATE TABLE IF NOT EXISTS Player(
   idPlayer INT PRIMARY KEY DEFAULT nextval('player_id_seq'),
   inteligencia INT NOT NULL,
   resistencia INT NOT NULL,
@@ -187,7 +187,7 @@ CREATE TABLE Player(
   FOREIGN KEY (fk_cyberLutador) REFERENCES CyberLutador (idCyberLutador)
 );
 
-CREATE TABLE Inimigo(
+CREATE TABLE IF NOT EXISTS Inimigo(
   idInimigo INT PRIMARY KEY DEFAULT nextval('inimigo_id_seq'),
   inteligencia INT NOT NULL,
   resistencia INT NOT NULL,
@@ -199,7 +199,7 @@ CREATE TABLE Inimigo(
   FOREIGN KEY (fk_cyberLutador) REFERENCES CyberLutador (idCyberLutador)
 );
 
-CREATE TABLE InstanciaInimigo (
+CREATE TABLE IF NOT EXISTS InstanciaInimigo (
   idInstanciaInimigo INT PRIMARY KEY DEFAULT nextval('instancia_inimigo_id_seq'),
   fk_inimigo INT NOT NULL,
   fk_player INT NOT NULL,
@@ -207,49 +207,49 @@ CREATE TABLE InstanciaInimigo (
   FOREIGN KEY (fk_player) REFERENCES Player (idPlayer)
 );
 
-CREATE TABLE NetRunners (
+CREATE TABLE IF NOT EXISTS NetRunners (
   idNetRunners INT PRIMARY KEY DEFAULT nextval('netrunners_id_seq'),
   fk_faccao INT NOT NULL,
   FOREIGN KEY (fk_faccao) REFERENCES Faccao (idFaccao)
 );
 
-CREATE TABLE CodeKeepers (
+CREATE TABLE IF NOT EXISTS CodeKeepers (
   idCodeKeepers INT PRIMARY KEY DEFAULT nextval('codekeepers_id_seq'),
   fk_faccao INT NOT NULL,
   FOREIGN KEY (fk_faccao) REFERENCES Faccao (idFaccao)
 );
 
-CREATE TABLE VoidWalkers (
+CREATE TABLE IF NOT EXISTS VoidWalkers (
   idVoidWalkers INT PRIMARY KEY DEFAULT nextval('voidwalkers_id_seq'),
   fk_faccao INT NOT NULL,
   FOREIGN KEY (fk_faccao) REFERENCES Faccao (idFaccao)
 );
 
-CREATE TABLE Implante (
+CREATE TABLE IF NOT EXISTS Implante (
   idImplante INT PRIMARY KEY DEFAULT nextval('implante_id_seq'),
   nomeImplante VARCHAR(50) NOT NULL,
   tipo VARCHAR(50) NOT NULL,
   localInstalacao VARCHAR(50) NOT NULL,
-  upgrade BOOLEAN NOT NULL
+  upgrade BOOLEAN NOT NULL,
   fk_cyberlutador INT NOT NULL,
-  FOREIGN KEY (fk_cyberlutador) REFERENCES CyberLutador (idCyberLadder)
+  FOREIGN KEY (fk_cyberlutador) REFERENCES CyberLutador (idCyberLutador)
 );
 
-CREATE TABLE VisaoCibernetica (
+CREATE TABLE IF NOT EXISTS VisaoCibernetica (
   fk_implante INT PRIMARY KEY,
   aumentaFurtividade BOOLEAN NOT NULL,
   aumentaPercepcao BOOLEAN NOT NULL,
   FOREIGN KEY (fk_implante) REFERENCES Implante (idImplante) ON DELETE CASCADE
 );
 
-CREATE TABLE BracoRobotico (
+CREATE TABLE IF NOT EXISTS BracoRobotico (
   fk_implante INT PRIMARY KEY,
   aumentaVida BOOLEAN NOT NULL,
   aumentaResistencia BOOLEAN NOT NULL,
   FOREIGN KEY (fk_implante) REFERENCES Implante (idImplante) ON DELETE CASCADE
 );
 
-CREATE TABLE CapaceteNeural (
+CREATE TABLE IF NOT EXISTS CapaceteNeural (
   fk_implante INT PRIMARY KEY,
   aumentaVelocidade BOOLEAN NOT NULL,
   aumentaInteligencia BOOLEAN NOT NULL,
