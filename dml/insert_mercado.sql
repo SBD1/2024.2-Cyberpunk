973d667a2fefcaa93508f7abb8261235cf0778fe
Lucas Meireles <98045972+Katuner@users.noreply.github.com>
2025-02-02 18:34:59 -0300

    Adiciona instanciaItem Pacote de Dados ao mercado

--- dml/insert_mercado.sql
+++ dml/insert_mercado.sql
@@ -2,6 +2,7 @@ INSERT INTO MercadoClandestino  (nomeMercado, fk_sala)
 VALUES ('CyberMercado', (SELECT idSala FROM Sala WHERE nomeSala = 'Cyber Mercado' LIMIT 1))
 WHERE NOT EXISTS (SELECT 1 FROM MercadoClandestino WHERE nomeMercado = 'CyberMercado');
 
-INSERT INTO InstanciaItem (fk_item, fk_mercado_clandestino)
-VALUES ((SELECT idItem FROM Item WHERE nomeItem = 'Pacote de Dados'), (SELECT idMercadoClandestino FROM MercadoClandestino WHERE nomeMercado = 'CyberMercado' LIMIT 1));
+INSERT INTO Mercado_Item (fk_mercado_clandestino, fk_instanciaitem)
+VALUES ((SELECT idMercadoClandestino FROM MercadoClandestino WHERE nomeMercado = 'CyberMercado' LIMIT 1), (SELECT idInstanciaItem FROM InstanciaItem WHERE fk_item = (SELECT idItem FROM Item WHERE nomeItem = 'Pacote de Dados') LIMIT 1))
+WHERE NOT EXISTS (SELECT 1 FROM Mercado_Item WHERE fk_mercado_clandestino = (SELECT idMercadoClandestino FROM MercadoClandestino WHERE nomeMercado = 'CyberMercado' LIMIT 1) AND fk_instanciaitem = (SELECT idInstanciaItem FROM InstanciaItem WHERE fk_item = (SELECT idItem FROM Item WHERE nomeItem = 'Pacote de Dados') LIMIT 1));
 
