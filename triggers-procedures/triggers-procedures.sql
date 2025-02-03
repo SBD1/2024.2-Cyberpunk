7db320aae5c6beee87ca5754217e31342405f900
Lucas Meireles <98045972+Katuner@users.noreply.github.com>
2025-02-03 14:40:45 -0300

    Correções triggers e procedures

--- triggers-procedures/triggers-procedures.sql
+++ triggers-procedures/triggers-procedures.sql
@@ -1,24 +1,29 @@
-ALTER TABLE Mochila ADD COLUMN dinheiro INT DEFAULT 0;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name = 'mochila' AND column_name = 'dinheiro') THEN
+        ALTER TABLE Mochila ADD COLUMN dinheiro INT DEFAULT 0;
+    END IF;
+END $$;
 
 CREATE FUNCTION recompensa_normal() RETURNS TRIGGER AS $recompensa_normal$
+DECLARE
+    valor_recompensa INT;
 BEGIN
-    DECLARE
-        valor_recompensa INT;
-    BEGIN
-        SELECT valor INTO valor_recompensa
-        FROM Recompensa
-        WHERE idInstanciaInimigo = OLD.idInstanciaInimigo;
 
-        IF valor_recompensa IS NULL THEN
-            valor_recompensa := 0; 
-        END IF;
+    SELECT valor INTO valor_recompensa
+    FROM Recompensa
+    WHERE fk_instancia_inimigo = OLD.idInstanciaInimigo;
 
-        UPDATE Mochila
-        SET dinheiro = dinheiro + valor_recompensa
-        WHERE fk_cyberlutador = OLD.idCyberLutador;
+    IF valor_recompensa IS NULL THEN
+        valor_recompensa := 0; 
+    END IF;
 
-        RETURN NEW;
-    END;
+    UPDATE Mochila
+    SET dinheiro = dinheiro + valor_recompensa
+    WHERE fk_cyberlutador = OLD.fk_cyberlutador;
+
+    RETURN OLD;
 END;
 $recompensa_normal$ LANGUAGE plpgsql;
 
@@ -27,19 +32,16 @@ BEFORE DELETE ON InstanciaInimigo
 FOR EACH ROW EXECUTE FUNCTION recompensa_normal();
 
 
+
 CREATE FUNCTION recompensa_missao() RETURNS TRIGGER AS $recompensa_missao$
 BEGIN
-    DECLARE
-        valor_recompensa INT;
-    BEGIN
-        IF NEW.progresso = 'COMPLETA' THEN
-            UPDATE Mochila
-            SET dinheiro = dinheiro + 50
-            WHERE fk_cyberlutador = NEW.fk_cyberlutador;     
-        END IF;
+    IF NEW.progresso = 'COMPLETA' AND OLD.progresso <> 'COMPLETA' THEN
+        UPDATE Mochila
+        SET dinheiro = dinheiro + 50
+        WHERE fk_cyberlutador = NEW.fk_cyberlutador;     
+    END IF;
 
-        RETURN NEW;
-    END;
+    RETURN NEW;
 END;
 $recompensa_missao$ LANGUAGE plpgsql;
 
@@ -47,30 +49,34 @@ CREATE TRIGGER recompensa_missao_trigger
 AFTER UPDATE ON Missao
 FOR EACH ROW EXECUTE FUNCTION recompensa_missao();
 
+
 CREATE FUNCTION comprar_item() RETURNS TRIGGER AS $comprar_item$
+DECLARE
+    valor_item INT;
+    saldo_atual INT;
 BEGIN
-    DECLARE
-        valor_item INT;
-    BEGIN
-        SELECT valor INTO valor_item
-        FROM Item i
-        JOIN InstanciaItem ii ON idInstanciaItem = NEW.fk_instanciaitem
-        WHERE idItem = NEW.fk_iditem;
+    SELECT i.valor INTO valor_item
+    FROM Item i
+    JOIN InstanciaItem ii ON ii.fk_item = i.idItem
+    WHERE ii.idInstanciaItem = NEW.fk_instanciaitem;
 
-        UPDATE Mochila
-        SET dinheiro = dinheiro - valor_item
-        WHERE fk_cyberlutador = NEW.fk_cyberlutador;
+    SELECT dinheiro INTO saldo_atual
+    FROM Mochila
+    WHERE fk_cyberlutador = NEW.fk_cyberlutador;
 
-        IF (dinheiro < 0) THEN
-            RAISE EXCEPTION 'Não há dinheiro suficiente para comprar o item';
-        END IF;
+    IF saldo_atual < valor_item THEN
+        RAISE EXCEPTION 'Não há dinheiro suficiente para comprar o item';
+    END IF;
 
-        RETURN NEW;
-    END;
+    UPDATE Mochila
+    SET dinheiro = dinheiro - valor_item
+    WHERE fk_cyberlutador = NEW.fk_cyberlutador;
+
+    RETURN NEW;
 END;
-
 $comprar_item$ LANGUAGE plpgsql;
 
+-- Criando o trigger
 CREATE TRIGGER comprar_item_trigger
 BEFORE UPDATE ON Mercado_Item
 FOR EACH ROW EXECUTE FUNCTION comprar_item();
